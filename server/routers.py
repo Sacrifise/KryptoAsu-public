425a0f293bf3df896f9bfadc458afc9e20b1b4a7
Kris <Hoakin037@gmail.com>
2025-04-17 02:37:53 +0700

    feat: add cifers endpoints

--- server/routers.py
+++ server/routers.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
-from schemas import GreaterCommonDivisor, LeastCommonMultiple, ModuleСomparison
+import schemas
 from functools import reduce
 from math import gcd, lcm
 from functions import solve_congruence
+import cifers
 
 router = APIRouter(
     prefix='/operations',
@@ -14,7 +15,7 @@ router = APIRouter(
     summary="нод"
 )
 async def greater_common_divisor(
-    response: GreaterCommonDivisor
+    response: schemas.GreaterCommonDivisor
 ):
     result = reduce(gcd, response.elements)
     return {
@@ -29,7 +30,7 @@ async def greater_common_divisor(
     summary="нок"
 )
 async def least_common_multiple(
-    response: LeastCommonMultiple
+    response: schemas.LeastCommonMultiple
 ):
     result = reduce(lcm, response.elements)
     return {
@@ -44,20 +45,102 @@ async def least_common_multiple(
     summary="модуль"
 )
 async def module_comprasion(
-    response: ModuleСomparison
+    response: schemas.ModuleСomparison
 ):
     result = solve_congruence(response.elements)
     if result is None:
         return {
             "success": True,
-            "type": "модуль",
+            "type": "Модуль",
             "value": response.elements,
             "result": "Решений нет"
         }
     else:
         return {
             "success": True,
-            "type": "модуль",
+            "type": "Модуль",
             "value": response.elements,
             "result": result
         }
+        
+        
+@router.post(
+    "/cifers/atbash",
+    summary="атбаш"
+)
+async def cifer_atbash(
+    response: schemas.AtbashCifer
+):
+    answer = cifers.atbash_cifer(response.text)
+    if response.operation == "encryption":
+        return {
+            "success": True,
+            "type": "Атбаш",
+            "operation": "Шифровка",
+            "value": response.text,
+            "result": answer
+        }
+    elif response.operation == "decryption":
+        return {
+            "success": True,
+            "type": "Атбаш",
+            "operation": "Дешифровка",
+            "value": response.text,
+            "result": answer
+        }
+        
+@router.post(
+    "/cifers/caesar",
+    summary="цезарь"
+)
+async def cifer_caesar(
+    response: schemas.CaesarCifer
+):
+    answer = cifers.caesar_cifer(response.text, response.shift )
+    
+    if response.operation == "encryption":
+        return {
+            "success": True,
+            "type": "Цезарь",
+            "operation": "Шифровка",
+            "shift": response.shift,
+            "value": response.text,
+            "result": answer
+        }
+    elif response.operation == "decryption":
+        return {
+            "success": True,
+            "type": "Цезарь",
+            "operation": "Дешифровка",
+            "shift": response.shift,
+            "value": response.text,
+            "result": answer
+        }
+        
+@router.post(
+    "/cifers/polybius",
+    summary="полибий"
+)
+async def cifer_atbash(
+    response: schemas.PolybiusCifer
+):
+    
+    if response.operation == "encryption":
+        answer = cifers.encrypt_polybius_cipher(response.text)
+        return {
+            "success": True,
+            "type": "Полибий",
+            "operation": "Шифровка",
+            "value": response.text,
+            "result": answer
+        }
+    elif response.operation == "decryption":
+        answer = cifers.decrypt_polybius_cipher(response.text)
+        return {
+            "success": True,
+            "type": "Полибий",
+            "operation": "Дешифровка",
+            "value": response.text,
+            "result": answer
+        }
+
